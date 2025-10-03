440d5432b2c45515a26a29d12a3894b245da0b06
Ritu Verma <77578748+rittuverma@users.noreply.github.com>
2025-10-03 16:36:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // ================= MIDDLEWARE =================
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({
+  origin: ["http://localhost:3000", "https://www.ajumanholidays.com"],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(express.json());
 
